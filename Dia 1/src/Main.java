2e046c08639511d0014a1fc9af529e1c5b5b934a
Jose Breno Sousa Jacauna <josebrenosousa@gmail.com>
2024-05-18 12:00:58 -0300

    java main Udemy

--- Dia 1/src/Main.java
+++ Dia 1/src/Main.java
@@ -1,11 +1,18 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 //nesse caso temos o package java util, default que tem o methodo MAIN
 //abaixo temos a class
 public class Main { //public pode ser acessada por outra parte do nosso codigo
 //esse e nosso method
     public static void main(String[] args ) { //metodo que nao retorna nada, VOID
         //aqui temos o statements(o que ira ser codificado)
-        System.out.print("Hello and welcome!");
-
+        Scanner scamNumber = new Scanner(System.in);
+        System.out.println("Digite um numero: ");
+        int numberOne = scamNumber.nextInt();
+        System.out.println("Digite outro numero: ");
+        int numberTwo = scamNumber.nextInt();
+        System.out.println("Resultado: "+(numberOne + numberTwo));
 
     }
 }
